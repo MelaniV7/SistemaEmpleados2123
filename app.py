883fa4a8bc9d247638ec254e4ba89655607ec616
melanivila <melanib.vila@gmail.com>
2021-07-13 10:56:15 -0300

    se agrego carpeta para guardar las fotos

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from logging import debug
 from flask import Flask
 from flask import render_template , request
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app= Flask(__name__)
 
@@ -32,8 +33,17 @@ def storage():
     _correo=request.form['txtCorreo']
     _foto=request.files['txtFoto']
 
+
+    now=datetime.now()
+    tiempo=now.strftime("%Y%H%M%S")
+
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+
     sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
-    datos=(_nombre , _correo , _foto.filename)
+    datos=(_nombre , _correo , nuevoNombreFoto)
 
     conn=mysql.connect()
     cursor=conn.cursor()
