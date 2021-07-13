e569786f7a80bf77af56f197872eb1be3a547b1f
melanivila <melanib.vila@gmail.com>
2021-07-13 10:42:13 -0300

    se agrego el create html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from logging import debug
 from flask import Flask
-from flask import render_template
+from flask import render_template , request
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -12,16 +12,36 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema2123'
 mysql.init_app(app)
 
-#esto es un comentario de prueba creado para la nueva rama
 @app.route('/')
 def index():
-    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'ejemplo', 'ejemplito@hotmail.com', 'foto-ejemplo.jpg')"
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'messi', 'messi@hotmail.com', 'foto-messi.jpg')"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
     conn.commit()
 
     return render_template('empleados/index.html')
+
+@app.route('/create')
+def create():
+   return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
+    datos=(_nombre , _correo , _foto.filename)
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return render_template('empleados/index.html')
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
