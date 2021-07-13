f640d35c897195a2a6c37527856ccbcb7b12bc94
melanivila <melanib.vila@gmail.com>
2021-07-13 11:46:21 -0300

    crud casi completo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from logging import debug
 from flask import Flask
-from flask import render_template , request
+from flask import render_template , request , redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
 
@@ -15,13 +15,48 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'messi', 'messi@hotmail.com', 'foto-messi.jpg')"
+    sql="SELECT * FROM `empleados`;"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
     conn.commit()
+    empleados=cursor.fetchall()
+    
+    return render_template('empleados/index.html', empleados=empleados)
 
-    return render_template('empleados/index.html')
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM empleados WHERE id=%s", (id))
+    conn.commit()
+    return redirect('/')
+
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id=%s", (id))
+    empleados=cursor.fetchall()
+    conn.commit()
+    return render_template('empleados/edit.html', empleados=empleados)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+    id=request.form['txtId']
+
+    sql="UPDATE empleados SET nombre=%s, correo=%s WHERE id=%s;"
+    datos=(_nombre, _correo,id)
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')
+
 
 @app.route('/create')
 def create():
@@ -50,7 +85,6 @@ def storage():
     cursor.execute(sql, datos)
     conn.commit()
     return render_template('empleados/index.html')
-
 
 
 if __name__=='__main__':
