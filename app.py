30f473b33a1069ad0e3cfec7bd368873a1476085
melanivila <melanib.vila@gmail.com>
2021-07-19 19:34:59 -0300

    modificar fotos

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template , request , redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app= Flask(__name__)
 
@@ -12,6 +13,10 @@ app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema2123'
 mysql.init_app(app)
+
+CARPETA = os.path.join('uploads')
+app.config['CARPETA']=CARPETA
+
 
 @app.route('/')
 def index():
@@ -53,6 +58,21 @@ def update():
     datos=(_nombre, _correo,id)
     conn=mysql.connect()
     cursor=conn.cursor()
+
+    now = datetime.now()
+    tiempo=now.strftime('%Y%H%M%S')
+    
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+        fila=cursor.fetchall()
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s", (nuevoNombreFoto, id))
+        conn.commit()
+
+
     cursor.execute(sql, datos)
     conn.commit()
     return redirect('/')
